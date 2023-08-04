073357a07d31cfd8a985ddf27b797c57d0a7592e
alakhpandya <alakhpandya@gmail.com>
2023-08-04 16:31:52 +0530

    aug4

---
+++ CSS/project4_travel_blog/js/main.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    navText: ['<i class="fa-solid fa-arrow-left-long"></i>','<i class="fa-solid fa-arrow-right-long"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
